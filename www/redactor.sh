c261e8984d0f30a7d759f907f19eecdc613c223e
Peedy2495 <you@example.com>
2023-02-07 18:51:05 +0100

    network issues solved

---
+++ www/redactor.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+exePath="$(dirname -- "${BASH_SOURCE[0]}")"
+
+# This tool was made for hiding sensitive informations before committing.
+# Refer these keys and paths for setting up your favours.
+
+
+# redact ssh keys
+sed -i 's/\(AAAA\).*/\1\<redacted\>"/' $exePath/www/user-data
+
+# redact passwords
+sed -i 's/\(password:\).*/\1 "\<redacted\>"/' $exePath/www/user-data 
+
+# redact domain setting
+sed -i 's/\(domain=\).*/\1"example.com"/' $exePath/www/assets/hostValues
+
+# redact mac settings
+sed -i 's/\(00:\).*/\1\<redacted\>/' $exePath/www/assets/hostValues
+
+# redact ip settings
+sed -i 's/\(168.\).*/\1\<redacted\>/' $exePath/www/assets/hostValues
